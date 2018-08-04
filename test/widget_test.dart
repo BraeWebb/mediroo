e9fa38b2aec1dda4a9554357eed95d4a91c4c44d
BraeWebb <email@braewebb.com>
2018-08-04 11:35:54 +1000

    Extended Tests
    Extended the tests for my own learning.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mediroo/routes.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increment by 1', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MediRooApp());
 
@@ -19,5 +19,31 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Counter increment by 20', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new MediRooApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    for (int i = 1; i < 50; i++) {
+      expect(find.text(i.toString()), findsNothing);
+    }
+
+    // Tap the '+' icon 20 times and trigger a frame.
+    for (int i = 0; i < 20; i++) {
+      await tester.tap(find.byIcon(Icons.add));
+    }
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    for (int i = 0; i < 50; i++) {
+      if (i == 20) {
+        expect(find.text(i.toString()), findsOneWidget);
+      } else {
+        expect(find.text(i.toString()), findsNothing);
+      }
+    }
   });
 }
