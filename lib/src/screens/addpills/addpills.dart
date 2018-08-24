489e7512603e65adeab067364ad85d55c09b2025
Louloutahooloo <l.woodrow@uqconnect.edu.au>
2018-08-24 13:50:57 +1000

    added an add pills page

--- lib/src/screens/addpills/addpills.dart
+++ lib/src/screens/addpills/addpills.dart
@@ -6,14 +6,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 ///
 /// Renders a click counter activated by a '+' FAB.
 class AddPillsPage extends StatefulWidget {
-  AddPillsPage({Key key, this.title}) : super(key: key);
-
-  /// The title to be displayed in the menu bar.
-  final String title;
-  static String tag = "HomePage";
+  AddPillsPage({Key key}) : super(key: key);
 
   @override
-  _HomePageState createState() => new _HomePageState();
+  _TempState createState() => new _TempState();
 }
 
 
@@ -21,18 +17,33 @@ class Pill {
   var _setTime;
   var _dayInterval;
   String _pillName;
+  //Pill(this._setTime, this._dayInterval, this._pillName);
 }
 
 
-class _HomePageState extends State<HomePage> {
+
+
+class _TempState extends State<AddPillsPage> {
+
+  /// The title to be displayed in the menu bar.
+  final String title = "Add Pills";
+  static String tag = "Addpill";
+
+  List<DropdownMenuItem<Text>> items = new List<DropdownMenuItem<Text>>();
+  //BuildContext context;
+  Scaffold scaffold;
+  ValueChanged<Text> val;
+  final pillFieldController = TextEditingController();
+
 
   bool _isFormComplete(){
     //TODO
-    return true;
+    return false;
   }
 
   Pill _getInfo(){
     //TODO
+    String pillName = pillFieldController.text;
     return new Pill();
   }
 
@@ -43,7 +54,10 @@ class _HomePageState extends State<HomePage> {
 
   void _addPill(){
     if (!_isFormComplete()){
-      //TODO register action
+      //TODO this doesn't work properly
+        Scaffold.of(this.context).showSnackBar(new SnackBar(
+        content: new Text("please complete the form properly"),
+      ));
       return;
     }
     Pill pill = _getInfo();
@@ -52,27 +66,47 @@ class _HomePageState extends State<HomePage> {
 
 
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have taken this many pills:',
-            ),
-            new Text(
-              'dude...',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    pillFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //this.context = context.currentContext();
+    items = new List<DropdownMenuItem<Text>>();
+    items.add(new  DropdownMenuItem(child: new Text('daily')));
+    items.add(new  DropdownMenuItem(child: new Text('weekly')));
+    items.add(new  DropdownMenuItem(child: new Text('fortnightly')));
+    return this.scaffold =  new Scaffold (
+        appBar: new AppBar(
+          title: new Text(title),
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _addPill,
-        tooltip: 'Increment',
-        child: new Icon(FontAwesomeIcons.prescriptionBottleAlt),
-      ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                'What pill needs to be Added:',
+              ),
+              new TextField(
+                controller: pillFieldController
+              ),
+              new Text(
+                  '\nHow often does this need to be taken?'
+              ),
+              new DropdownButton(
+                  items: items, onChanged: val
+              ),
+              new Text(
+                  '\n'
+              ),
+              new RaisedButton(onPressed: _addPill, child: new Text('add pill'))
+            ],
+          ),
+        )
     );
   }
 }
