603b8843f748e740f468d16aaf4722fa77534718
BraeWebb <email@braewebb.com>
2018-09-10 21:43:17 +1000

    Login Authentication

--- lib/src/util/database/user.dart
+++ lib/src/util/database/user.dart
@@ -2,9 +2,16 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 /// Get the User UID for the user currently logged in
 Future<String> currentUUID() async {
+  if (_auth.currentUser() != null) {
+    return (await _auth.currentUser()).uid;
+  }
+
   Stream<QuerySnapshot> snapshots = Firestore.instance.collection('tests').snapshots();
 
   QuerySnapshot snapshot = await snapshots.first;
--- lib/util.dart
+++ lib/util.dart
@@ -3,3 +3,4 @@
 export 'src/util/database/prescriptions.dart';
 export 'src/util/database/user.dart';
 export 'src/util/testing.dart';
+export 'src/util/authentication.dart';
