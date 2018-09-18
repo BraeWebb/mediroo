fd8f6e6c812b9bdb6965160248d265e30d95049e
BraeWebb <email@braewebb.com>
2018-09-18 15:21:24 +1000

    Pillbox Removed

--- mediroo/lib/screens.dart
+++ mediroo/lib/screens.dart
@@ -1,7 +1,5 @@
 /// Application window screens
 
-export 'src/screens/pillbox/pillbox.dart';
-export 'src/screens/pillbox/pill_info.dart';
 export 'src/screens/pillbox/add_pills.dart';
 export 'src/screens/pillbox/pill_list.dart';
 export 'src/screens/login/login.dart';
