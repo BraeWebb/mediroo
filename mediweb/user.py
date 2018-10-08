c9bfcbef9045fa170df35b233a1a9bb9919864d5
BraeWebb <email@braewebb.com>
2018-10-08 22:23:28 +1000

    Fixed Unregistered Users

--- mediweb/user.py
+++ mediweb/user.py
@@ -2,13 +2,16 @@ from database import find_user, add_user
 
 
 class User:
-    def __init__(self, user_id):
-        self.name, self.email, self.authorized = find_user(user_id)
+    def __init__(self, user_id, name=None, email=None):
+        if user_id is not None:
+            self.name, self.email, self.authorized = find_user(user_id)
+        else:
+            self.name, self.email, self.authorized = name, email, False
 
-    @staticmethod
-    def register(payload):
-        add_user(payload)
+    @classmethod
+    def register(cls, payload):
+        return cls(None, name=payload['name'], email=payload['email'])
 
     @staticmethod
     def is_registered(user_id):
-        return True
+        return find_user(user_id) is not None
