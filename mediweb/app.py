b4c5edc78f31c1c5e5094403bfa22fdb800922d2
BraeWebb <email@braewebb.com>
2018-10-08 21:30:44 +1000

    Removed Idea Files

--- mediweb/app.py
+++ mediweb/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template as render
 
 from uqauth import global_user, login_required, authorized_access
-from uqauth import payload_view, login
+from uqauth import payload_view, login, logout
 
 app = Flask(__name__)
 app.secret_key = 'placeholder secret'
@@ -10,11 +10,12 @@ app.url_map.strict_slashes = False
 
 app.add_url_rule('/admin/payload', 'payload', payload_view)
 app.add_url_rule('/admin/login', 'login', login)
+app.add_url_rule('/admin/logout', 'logout', logout)
 
 
 @app.route('/landing')
 def index():
-    return 'Hello World! It be me!'
+    return render('semantic-ui/index.html')
 
 
 @app.route('/admin')
