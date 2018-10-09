7206c2e7d2cec4c7e5815d776828ab353bce4294
BraeWebb <email@braewebb.com>
2018-10-09 13:38:16 +1000

    Persistant Data

--- mediweb/app.py
+++ mediweb/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template as render
+from flask import Flask, render_template as render, request, jsonify
 
 from uqauth import global_user, login_required, authorized_access
 from uqauth import payload_view, login, logout
+from database import send_purchase
 
 app = Flask(__name__)
 app.secret_key = 'placeholder secret'
@@ -17,9 +18,17 @@ app.add_url_rule('/admin/logout', 'logout', logout)
 def landing():
     return render('landing.html')
 
+
 @app.route('/purchase')
 def purchase():
     return render('purchase.html')
+
+
+@app.route('/purchase/submit', methods=["POST"])
+def submit_purchase():
+    send_purchase(request.form.get('name'), request.form.get('email'), request.form.get('comments'))
+    return jsonify({"message": "Submitted Data"})
+
 
 @app.route('/admin')
 @authorized_access
